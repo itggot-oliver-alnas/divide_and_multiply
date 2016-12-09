acb346fa9acc22e55af2284a990120c67bd18712
itggot-oliver-alnas <oliver.alnas@itggot.se>
2016-12-09 12:21:25 +0100

    Funktion klar
    Funktionen är färdig skriven och rake test ger PASS på alla krav.

--- lib/divide_and_multiply.rb
+++ lib/divide_and_multiply.rb
@@ -1,10 +1,14 @@
 def divide_and_multiply(value: )
-  array = [value, ]
+  array = [value]
   while value != 1
-    if value == value % 2 = 0
+    if value % 2 == 0
       value /= 2
+      array << value
     else
+      value *= 3
+      value += 1
+      array << value
+    end
   end
-
-  end
+  return array
 end
--- main.rb
+++ main.rb
@@ -1,1 +1,3 @@
 require_relative 'lib/divide_and_multiply'
+
+puts divide_and_multiply(value: 80)
